536b17db3f1bf25701737773e755527f1766d363
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2019-02-18 16:05:19 +0100

    Route to generate a random hand

---
+++ src/AppBundle/Controller/PokerController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PokerController extends Controller
+{
+    /**
+     * @Route("/poker/random-hand/{player}", name="randomHand")
+     */
+    public function randomHandAction(string $player) {
+
+        $generator = $this->container->get('PokerRandomHandGenerator');
+        $hand = $generator($player);
+
+        return new Response(
+            "hand " . $hand->__toString() . " " .
+            "by " . $hand->getPlayer() . " " .
+            "at " . $hand->getAt()->format("Y-m-d H:i:s")
+        );
+    }
+}
